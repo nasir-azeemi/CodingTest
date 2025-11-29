581acb16eec931858809662ba9af34a81bd0013c
Agha Syed Nasir Mahmood Azeemi <54790791+nasir-azeemi@users.noreply.github.com>
2025-11-29 10:29:49 +0500

    levenshtein

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,45 @@ const app = express();
 
 const PORT = 3000;
 
+const levenshtein = (str1, str2) => {
+  const m = str1.length;
+  const n = str2.length;
+
+  const dp = new Array(m + 1).fill(null).map(() => new Array(n + 1).fill(0));
+
+  // Initialize the first row
+  // and column of the matrix
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i;
+  }
+
+  for (let j = 0; j <= n; j++) {
+    dp[0][j] = j;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (str1[i - 1] === str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] =
+          1 +
+          Math.min(
+            // Insert
+            dp[i][j - 1],
+            Math.min(
+              // Remove
+              dp[i - 1][j],
+              // Replace
+              dp[i - 1][j - 1]
+            )
+          );
+      }
+    }
+  }
+  return dp[m][n];
+};
+
 app.use(express.json()); // So Express know you're using JSON
 
 app.get("/", (request, response) => {
@@ -17,6 +56,30 @@ app.get("/api/users", async (request, response) => {
   const users = res.data;
 
   return response.send(users);
+});
+
+app.get("/api/search", async (request, response) => {
+  const { q } = request.query;
+
+  const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+  const users = res.data;
+
+  const distances = users.map((u) => {
+    const id = u.id;
+    const userName = u.name;
+
+    return { id, name: userName, distance: levenshtein(userName, q) };
+  });
+
+  // sort descending
+  distances.sort((a, b) => {
+    return parseFloat(b.distance) - parseFloat(a.distance);
+  });
+
+  const top5 = distances.slice(-5);
+
+  return response.send({ top5, distances });
 });
 
 app.listen(PORT, (error) => {
